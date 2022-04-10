b1f230ffc86855e1ad2e9a3704dcd0259027b5b0
Fredrik Sundberg <fredrik.sundberg@afry.com>
2022-04-11 00:22:27 +0200

    Path fix

--- __tests__/API.test.js
+++ __tests__/API.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest')
-
+const path = require('path')
 const app = require('../server')
 
 describe('File upload endpoints', () => {
   it('should process files as expected', async () => {
     const res = await request(app)
       .post('/upload')
-      .attach('data', '__tests__/data/wat.txt')
+      .attach('data', path.join(__dirname, 'data', 'wat.txt'))
 
     expect(res.text).toEqual('this fooisbar fooisbar a simple test')
     expect(res.statusCode).toEqual(200)
@@ -15,7 +15,7 @@ describe('File upload endpoints', () => {
   it('should handle multiple most popular words', async () => {
     const res = await request(app)
       .post('/upload')
-      .attach('data', '__tests__/data/tripple.txt')
+      .attach('data', path.join(__dirname, 'data', 'tripple.txt'))
 
     expect(res.text).toEqual('foohellobar foohellobar fooworldbar fooworldbar foobazbar foobazbar hej')
     expect(res.statusCode).toEqual(200)
@@ -24,7 +24,7 @@ describe('File upload endpoints', () => {
   it('handles empty file', async () => {
     const res = await request(app)
       .post('/upload')
-      .attach('data', '__tests__/data/empty.txt')
+      .attach('data', path.join(__dirname, 'data', 'empty.txt'))
 
     expect(res.text).toEqual('')
     expect(res.statusCode).toEqual(200)
