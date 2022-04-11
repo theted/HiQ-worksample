60761f72f40cc85df7cd5e1d721c310823a28df9
Fredrik Sundberg <fredrik.sundberg@afry.com>
2022-04-11 11:24:35 +0200

    Move readFile to utils, read file content in file upload controller

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,17 +1,26 @@
 const { uploadFiles } = require('../lib/file-upload.js')
-const { processFile } = require('../lib/file-processor.js')
+const { processFile, processText } = require('../lib/file-processor.js')
+const { readFile } = require('../lib/utils.js')
 
 const upload = async (req, res) => {
   if (!req.files) return res.status(400).send('No files were uploaded.')
+
+  // TODO: proper support for multiple files
   let files = req.files.data
+
   if (!Array.isArray(files)) files = [files]
 
   const allResults = await uploadFiles(files)
 
   console.log({ allResults })
 
+  // debug
+  const contents = await readFile(allResults[0])
+  console.log({contents})
+
   // TODO: support for multiple files
-  const output = await processFile(allResults[0])
+  const output = await processText(contents)
+  // const output = await processFile(allResults[0])
 
   return res.send(output)
 }
