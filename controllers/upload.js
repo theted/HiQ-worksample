c1766a552e54a819252d77271f680ac9d7e53277
Fredrik Sundberg <fredrik.sundberg@afry.com>
2022-04-17 00:50:46 +0200

    Support multiple file upload, only accept text files

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,25 +1,25 @@
-const { uploadFiles } = require('../lib/file-upload.js')
-const { processFile, processText } = require('../lib/file-processor.js')
-const { readFile } = require('../lib/utils.js')
+const { processText } = require('../lib/file-processor.js')
 
-const processRaw = async (files) => {
-  return files[0].data.toString()
+const ALLOWED_MIMES = [
+  'text/plain'
+]
+
+const processFiles = async (files) => {
+  const promises = files.map(file => processText(file.data.toString()))
+  return await Promise.all(promises)
 }
+
+// checking mimetype is not really a sane way of sanitizing files..
+const sanitizeFiles = files => files.filter(file =>
+  ALLOWED_MIMES.includes(file.mimetype)
+)
 
 const upload = async (req, res) => {
   if (!req.files) return res.status(400).send('No files were uploaded.')
-
-  // TODO: proper support for multiple files
-  let files = req.files.data
-
-  if (!Array.isArray(files)) files = [files]
-
-  const rawText = await processRaw(files)
-
-  // TODO: support for multiple files
-  const output = await processText(rawText)
-
-  return res.send(output)
+  const saneFiles = sanitizeFiles(Object.values(req.files))
+  if(!saneFiles.length) return res.send('No valid files uploaded')
+  const promisedResults = await processFiles(saneFiles)
+  return res.send(promisedResults.join('\n').trim())
 }
 
 module.exports = { upload }
