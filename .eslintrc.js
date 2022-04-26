a2d1a81fbdc9b7a66b0a78cf755111224d48a223
Fredrik Sundberg <fredrik.sundberg@afry.com>
2022-04-26 23:57:53 +0200

    Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "standard"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-};
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -2,27 +2,27 @@
 
 <script>
 
-  import Vue from "vue"
-  import FileUpload from "../file-upload/file-upload.vue"
-  import Result from "../Result/Result.vue"
+import Vue from 'vue'
+import FileUpload from '../file-upload/file-upload.vue'
+import Result from '../Result/Result.vue'
 
-  export default {
-    name: "app",
-    components: {
-      FileUpload,
-      Result
-    },
-    data() {
-      return {
-        items: []
-      }
-    },
-    methods: {
-      uploadEvent(content) {
-        this.items.push({content})
-      }
+export default {
+  name: 'app',
+  components: {
+    FileUpload,
+    Result
+  },
+  data () {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    uploadEvent (content) {
+      this.items.push({ content })
     }
   }
+}
 </script>
 
 <style src="./App.styl" lang="stylus"></style>
